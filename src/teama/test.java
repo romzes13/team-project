640218a9fecee88cc466361328ee515d68715232
Roman Developer <romzes13@gmail.com>
2017-11-20 01:26:22 -0500

    First commit.
    Signed-off-by: Roman

---
+++ src/teama/test.java
@@ -0,0 +1,23 @@
+package teama;
+
+
+public class test {
+
+	
+	public static void helloRoman() {
+		
+		System.out.println("Hello, I'm Roman =)");
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+
+		helloRoman();
+		
+		
+	}
+
+}

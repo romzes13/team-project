7d5c1d60a0429c05dcb439fdccce9adcf0909420
Roman Developer <romzes13@gmail.com>
2017-11-29 21:05:11 -0500

    Missed parenthesis in method.

--- src/teama/Sandbox.java
+++ src/teama/Sandbox.java
@@ -7,7 +7,7 @@ public class Sandbox {
     
     public static void main(String[] args){
         Customer customer = new Customer("John", "Doe");
-        Node node = new Node(customer.getFirstName(), customer.getLastName);
+        Node node = new Node(customer.getFirstName(), customer.getLastName());
         node.displayNode();
     }
 }

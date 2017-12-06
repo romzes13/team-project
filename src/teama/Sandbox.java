e6172822a404ff84c39919a0901792a1d6eace23
TomDabek <tomdabek97@gmail.com>
2017-12-06 17:14:39 -0500

    Testing

--- src/teama/Sandbox.java
+++ src/teama/Sandbox.java
@@ -11,6 +11,8 @@ public class Sandbox {
     
     public static void main(String[] args){
     	
+    	//Testing some methods on shuffling and displaying the customers. Feel free to delete if needed. 
+    	
     	long before = System.currentTimeMillis();
     	
     	CustomerArrayList caList = new CustomerArrayList();

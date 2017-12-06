647186423ecb33288e194264d094dfc3dddc3d62
TomDabek <tomdabek97@gmail.com>
2017-12-06 18:03:54 -0500

    Tiny product tweeks, testing.

--- src/teama/Sandbox.java
+++ src/teama/Sandbox.java
@@ -12,21 +12,15 @@ public class Sandbox {
     public static void main(String[] args){
     	
     	//Testing some methods on shuffling and displaying the customers. Feel free to delete if needed. 
-    	
     	long before = System.currentTimeMillis();
     	
-    	CustomerArrayList caList = new CustomerArrayList();
-    	caList.shuffleList();
-		ArrayList<Customer> custListAr = caList.getListOfCustomers();
-		
-		
-		for(Customer c : custListAr) {
-			System.out.println(c.toString());
-			
-		}
-		
-		long after = System.currentTimeMillis();
+    	ProductArrayList pl = new ProductArrayList();
+    	System.out.println(pl.getListOfProducts());
+    	
+    	long after = System.currentTimeMillis();
 		System.out.println("Time taken: " + (after - before) + " milliseconds\n");
+    	
+    	
     	
     }
 }

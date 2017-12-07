4a4d080749e726e172e5615106f5dd9442bae760
TomDabek <tomdabek97@gmail.com>
2017-12-07 15:42:33 -0500

    Testing new totalSpentPerCustomer method

--- src/teama/Sandbox.java
+++ src/teama/Sandbox.java
@@ -8,22 +8,21 @@ import teama.arraylist.CustomerArrayList;
  * Sandbox for testing class methods
  */
 public class Sandbox {
-    
-    public static void main(String[] args){
-    	
-    	//Testing some methods on shuffling and displaying the customers. Feel free to delete if needed. 
-    	long before = System.currentTimeMillis();
-    	
-    	CustomerArrayList caList = new CustomerArrayList();
+
+	public static void main(String[] args) {
+
+		// Testing some methods on shuffling and displaying the customers. Feel free to
+		// delete if needed.
+		long before = System.currentTimeMillis();
+
+		CustomerArrayList caList = new CustomerArrayList();
 		ArrayList<Customer> custListAr = caList.getListOfCustomers();
-		
+
 		caList.populatePurHistory();
 		caList.totalSpentPerCustomer(355);
-    	
-    	long after = System.currentTimeMillis();
+
+		long after = System.currentTimeMillis();
 		System.out.println("Time taken to add up orders: " + (after - before) + " milliseconds\n");
-    	
-    	
-    	
-    }
+
+	}
 }

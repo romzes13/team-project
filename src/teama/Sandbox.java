5b5e1f7688c1502894b0670ba55312a545f55042
TomDabek <tomdabek97@gmail.com>
2017-12-09 14:47:05 -0500

    Two unfinished methods, test.

--- src/teama/Sandbox.java
+++ src/teama/Sandbox.java
@@ -13,30 +13,11 @@ public class Sandbox {
 	
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
-		CustomerArrayList clist = new CustomerArrayList();
-		ArrayList<Customer> cr = clist.getListOfCustomers();
-=======
-		//CustomerArrayList clist = new CustomerArrayList();
-		//ArrayList<Customer> cr = clist.getListOfCustomers();
-		//clist.printCustomerList();
-	
->>>>>>> refs/remotes/origin/master
-		
-		
-		
-		
-	HashTable test = new HashTable();
-<<<<<<< HEAD
-	Hashtable<Customer, Customer> customerProduct = test.getTable();
-	test.printTable(test);
-	test.getCustomer("Adam");
-=======
-	Hashtable customerProduct = test.getTable();
-	//test.printValues(test);
-	//System.out.println(customerProduct);
-	test.printKeys(test);
->>>>>>> refs/remotes/origin/master
+		 HashTable test = new HashTable();
+
+			Hashtable customerProduct = test.getTable();
+
+			test.printTable(test);
 	
 	}
 }
